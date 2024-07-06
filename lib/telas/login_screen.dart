4c929d44356b96a142b3e3e4fae08c77ed17e6b4
fabiopereiraz <fabio.pereira@icarros.com.br>
2024-07-06 11:36:40 -0300

    criada tela de login

--- lib/telas/login_screen.dart
+++ lib/telas/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bingo_app_vendedor/telas/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -8,6 +9,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController loginController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,6 +19,58 @@ class _LoginScreenState extends State<LoginScreen> {
           leading: Container(),
           title: Text('Login tela'),
         ),
-        body: Text('Login  tela !!!'));
+        body: Container(
+          decoration: BoxDecoration(color: Colors.yellow),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Login '),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: loginController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: 'Digite o login',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('senha'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: passwordController,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    hintText: 'Digite a senha',
+                    fillColor: Colors.white70,
+                    filled: true,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print('clicou em Login');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (contextNew) => HomeScreen(),
+                    ),
+                  );
+                },
+                child: Text('Login'),
+              ),
+            ],
+          ),
+        ));
   }
 }
